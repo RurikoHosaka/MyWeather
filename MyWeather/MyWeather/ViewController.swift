60e650f4c1f39a1e73d0970e30f23b4eb09118a7
Ruriko Hosaka <hosaka@museit.co.jp>
2020-04-24 14:38:09 +0900

    create:リボジトリ

---
+++ MyWeather/MyWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by ruriko hosaka on 2020/04/24.
+//  Copyright © 2020 ruriko hosaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
