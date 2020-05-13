6913f184a41768f7e2e4415ba6c39a4d0f609816
Ruriko Hosaka <hosaka@museit.co.jp>
2020-05-13 17:18:02 +0900

    add:3時間ごとの天気の表示

--- MyWeather/MyWeather/ViewController.swift
+++ MyWeather/MyWeather/ViewController.swift
@@ -22,19 +22,33 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var region:String = ""
     var regionData:[String] = []
-    var details:[String] = []
     var dateString:String = ""
     var weatherURL:URL?
     var weather:String = ""
     var high:String = ""
     var low:String = ""
-    var tomorrowDateString:String = ""
-    var tomorrowWeatherURL:URL?
-    var tomorrowWeather:String = ""
-    var tomorrowHigh:String = ""
-    var tomorrowLow:String = ""
     var data:Data?
     var getCoordeFlag = 0
+    var timeFirst = ""
+    var timeSecond = ""
+    var timeThird = ""
+    var timeFourth = ""
+    var timeFifth = ""
+    var timeSixth = ""
+    var timeSeventh = ""
+    var timeEighth = ""
+    var timeNinth = ""
+    var timeTenth = ""
+    var weatherFirst:Data?
+    var weatherSecond:Data?
+    var weatherThird:Data?
+    var weatherFourth:Data?
+    var weatherFifth:Data?
+    var weatherSixth:Data?
+    var weatherSeventh:Data?
+    var weatherEighth:Data?
+    var weatherNinth:Data?
+    var weatherTenth:Data?
     
     //地域を扱うキーを設定
     let settingKey = "region_value"
@@ -50,13 +64,52 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     // user location
     let locationManager = CLLocationManager()
     
+    @IBOutlet weak var regionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var highLabel: UILabel!
+    @IBOutlet weak var lowLabel: UILabel!
+    @IBOutlet weak var firstWeatherLabel: UILabel!
+    @IBOutlet weak var secondWeatherLabel: UILabel!
+    @IBOutlet weak var thirdWeatherLabel: UILabel!
+    @IBOutlet weak var fourthWeatherLabel: UILabel!
+    @IBOutlet weak var fifthWeatherLabel: UILabel!
+    @IBOutlet weak var sixthWeatherLabel: UILabel!
+    @IBOutlet weak var seventhWeatherLabel: UILabel!
+    @IBOutlet weak var eighthWeatherLabel: UILabel!
+    @IBOutlet weak var ninthWeatherLabel: UILabel!
+    @IBOutlet weak var tenthWeatherLabel: UILabel!
+    @IBOutlet weak var firstWeatherImage: UIImageView!
+    @IBOutlet weak var secondWeatherImage: UIImageView!
+    @IBOutlet weak var thirdWeatherImage: UIImageView!
+    @IBOutlet weak var fourthWeatherImage: UIImageView!
+    @IBOutlet weak var fifthWeatherImage: UIImageView!
+    @IBOutlet weak var sixthWeatherImage: UIImageView!
+    @IBOutlet weak var seventhWeatherImage: UIImageView!
+    @IBOutlet weak var eighthWeatherImage: UIImageView!
+    @IBOutlet weak var ninthWeatherImage: UIImageView!
+    @IBOutlet weak var tenthWeatherImage: UIImageView!
+    
+    @IBAction func settingButtonAction(_ sender: Any) {
+        //画面遷移を行う
+        performSegue(withIdentifier:"goSetting", sender: nil)
+    }
+    
+    @IBAction func getCoordButtonAction(_ sender: Any) {
+        //現在地の緯度経度を取得する
+        getCoordeFlag = 1
+        setupLocationManager()
+        getWeatherData()
+        getWeatherDataEveryThreeHours()
+        getCoordeFlag = 0
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         //現在日付を取得
         getTodayDate()
-        
         //UserDefaultsのインスタンスを生成
         let settings = UserDefaults.standard
         //UserDefaultsに初期値を登録
@@ -69,37 +122,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.startUpdatingLocation()
         }
         setupLocationManager()
-        getWeatherData(row: 0)
-        getWeatherData(row: 1)
-        
+        getWeatherData()
+        getWeatherDataEveryThreeHours()
     }
     
-    @IBOutlet weak var regionLabel: UILabel!
-    @IBOutlet weak var detailsTextView: UITextView!
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var weatherImage: UIImageView!
-    @IBOutlet weak var weatherLabel: UILabel!
-    @IBOutlet weak var highLabel: UILabel!
-    @IBOutlet weak var lowLabel: UILabel!
-    @IBOutlet weak var tomorrowDateLabel: UILabel!
-    @IBOutlet weak var tomorrowWeatherImage: UIImageView!
-    @IBOutlet weak var tomorrowWeatherLabel: UILabel!
-    @IBOutlet weak var tomorrowHighLabel: UILabel!
-    @IBOutlet weak var tomorrowLowLabel: UILabel!
-    
-    @IBAction func settingButtonAction(_ sender: Any) {
-        //画面遷移を行う
-        performSegue(withIdentifier:"goSetting", sender: nil)
-    }
-    
-    @IBAction func getCoordButtonAction(_ sender: Any) {
-        //現在地の緯度経度を取得する
-        getCoordeFlag = 1
-        setupLocationManager()
-        getWeatherData(row: 0)
-        getWeatherData(row: 1)
-        getCoordeFlag = 0
-    }
     //現在日付を取得する処理
     private func getTodayDate() {
         let dt = Date()
@@ -110,12 +136,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     //APIから天気情報を取得する処理
-    private func getWeatherData(row: Int){
+    private func getWeatherData(){
         //UserDefaultsの取得
         let settings = UserDefaults.standard
         var regionValue = ""
         if getCoordeFlag == 0 {
             regionValue = settings.string(forKey: settingKey)!
+            //regionValue = "13"
         } else {
             regionValue = "0"
         }
@@ -130,6 +157,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             regionData[3] = String(lonLocation)
         }
         
+        //今日の天気予報を取得
         let url = "http://api.openweathermap.org/data/2.5/weather?lat=\(regionData[2])&lon=\(regionData[3])&appid=\(apiKey)&units=metric"
         
         //Alamofireで通信し、データを取得
@@ -142,19 +170,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 let json:JSON = JSON(response.data as Any)
                 
                 //取得したjsonから、必要なデータを取り出す
-                //let details = json["description"]["text"].string
-                let weather = json[["weather"]].array![0]["main"].stringValue
+                let weather = json["weather"].array![0]["main"].stringValue
                 let iconName = json["weather"].array![0]["icon"].stringValue
                 let high = Int(round(json["main"]["temp_max"].doubleValue))
                 let low = Int(round(json["main"]["temp_min"].doubleValue))
-                
-                //取り出したデータを、それぞれの変数に入れる
-                //データがない場合もあるので、その時は"No Data"と入れておく
-                //if details != nil {
-                //    self.details = (details?.components(separatedBy: .newlines))!  //改行で分割します。
-                //} else {
-                //    self.details = ["No Details"]
-                //}
                 
                 do {
                     let url = URL(string: "http://openweathermap.org/img/wn/\(iconName)@2x.png")
@@ -167,7 +186,80 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 self.low = "\(low)°"
                 
                 //ラベルに反映させる
-                self.setWeatherData(row: row)
+                self.setWeatherData(row:0)
+                
+            case .failure(let error):
+                print("-------- エラー ------")
+                print(error)
+            }
+        }
+    }
+    //APIから天気情報を取得する処理
+    private func getWeatherDataEveryThreeHours(){
+        //3時間ごとの天気予報を取得
+        let url = "http://api.openweathermap.org/data/2.5/forecast?lat=\(regionData[2])&lon=\(regionData[3])&appid=\(apiKey)&units=metric&cnt=10"
+        
+        //Alamofireで通信し、データを取得
+        AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON {(response) in
+            
+            switch response.result {
+            case .success:
+                
+                //jsonを取得
+                let json:JSON = JSON(response.data as Any)
+                var city:String = ""
+                for cell in 0..<10 {
+                    //取得したjsonから、必要なデータを取り出す
+                    let iconName = json["list"].array![cell]["weather"].array![0]["icon"].stringValue
+                    let time = json["list"].array![cell]["dt"].stringValue
+                    city = json["city"]["name"].stringValue
+                    // UNIX時間 "dateUnix" をNSDate型 "date" に変換
+                    let dateUnix: TimeInterval = TimeInterval(time)!
+                    let dt = NSDate(timeIntervalSince1970: dateUnix)
+                    // NSDate型を日時文字列に変換するためのNSDateFormatterを生成
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "H時"
+                    do {
+                        let url = URL(string: "http://openweathermap.org/img/wn/\(iconName)@2x.png")
+                        if cell == 0 {
+                            self.weatherFirst = try Data(contentsOf: url!)
+                            self.timeFirst = dateFormatter.string(from: dt as Date)
+                        }else if cell == 1 {
+                            self.weatherSecond = try Data(contentsOf: url!)
+                            self.timeSecond = dateFormatter.string(from: dt as Date)
+                        }else if cell == 2 {
+                            self.weatherThird = try Data(contentsOf: url!)
+                            self.timeThird = dateFormatter.string(from: dt as Date)
+                        }else if cell == 3 {
+                            self.weatherFourth = try Data(contentsOf: url!)
+                            self.timeFourth = dateFormatter.string(from: dt as Date)
+                        }else if cell == 4 {
+                            self.weatherFifth = try Data(contentsOf: url!)
+                            self.timeFifth = dateFormatter.string(from: dt as Date)
+                        }else if cell == 5 {
+                            self.weatherSixth = try Data(contentsOf: url!)
+                            self.timeSixth = dateFormatter.string(from: dt as Date)
+                        }else if cell == 6 {
+                            self.weatherSeventh = try Data(contentsOf: url!)
+                            self.timeSeventh = dateFormatter.string(from: dt as Date)
+                        }else if cell == 7 {
+                            self.weatherEighth = try Data(contentsOf: url!)
+                            self.timeEighth = dateFormatter.string(from: dt as Date)
+                        }else if cell == 8 {
+                            self.weatherNinth = try Data(contentsOf: url!)
+                            self.timeNinth = dateFormatter.string(from: dt as Date)
+                        }else if cell == 9 {
+                            self.weatherTenth = try Data(contentsOf: url!)
+                            self.timeTenth = dateFormatter.string(from: dt as Date)
+                        }
+                    }catch let err {
+                        print("Error : \(err.localizedDescription)")
+                    }
+                    
+                }
+                print(city)
+                //ラベルに反映させる
+                self.setWeatherData(row:1)
                 
             case .failure(let error):
                 print("-------- エラー ------")
@@ -178,30 +270,41 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     //ラベルに反映させる処理
     private func setWeatherData(row: Int) {
         
-        if row == 0 {
+        if row == 0{
             regionLabel.text = regionData[1] + " の 天気"
-            //分割時に配列になっているので、1つ目の要素だけ表示します。
-            //detailsTextView.text = details[0]
             dateLabel.text = "今日 " + dateString
             weatherImage.image = UIImage(data: data!)
             weatherLabel.text = weather
             highLabel.text = high
             lowLabel.text = low
-            
-        } else if row == 1 {
-            
-            tomorrowDateLabel.text = "明日 " + dateString
-            tomorrowWeatherImage.image = UIImage(data: data!)
-            tomorrowWeatherLabel.text = weather
-            tomorrowHighLabel.text = high
-            tomorrowLowLabel.text = low
+        } else if row == 1{
+            firstWeatherLabel.text = timeFirst
+            secondWeatherLabel.text = timeSecond
+            thirdWeatherLabel.text = timeThird
+            fourthWeatherLabel.text = timeFourth
+            fifthWeatherLabel.text = timeFifth
+            sixthWeatherLabel.text = timeSixth
+            seventhWeatherLabel.text = timeSeventh
+            eighthWeatherLabel.text = timeEighth
+            ninthWeatherLabel.text = timeNinth
+            tenthWeatherLabel.text = timeTenth
+            firstWeatherImage.image = UIImage(data: weatherFirst!)
+            secondWeatherImage.image = UIImage(data: weatherSecond!)
+            thirdWeatherImage.image = UIImage(data: weatherThird!)
+            fourthWeatherImage.image = UIImage(data: weatherFourth!)
+            fifthWeatherImage.image = UIImage(data: weatherFifth!)
+            sixthWeatherImage.image = UIImage(data: weatherSixth!)
+            seventhWeatherImage.image = UIImage(data: weatherSeventh!)
+            eighthWeatherImage.image = UIImage(data: weatherEighth!)
+            ninthWeatherImage.image = UIImage(data: weatherNinth!)
+            tenthWeatherImage.image = UIImage(data: weatherTenth!)
         }
     }
     //画面切り替えのタイミングで処理を行う
     override func viewDidAppear(_ animated: Bool) {
         //天気予報を更新する
-        _ = getWeatherData(row: 0)
-        _ = getWeatherData(row: 1)
+        _ = getWeatherData()
+        _ = getWeatherDataEveryThreeHours()
     }
     
     //現在地を取得する処理
@@ -222,4 +325,3 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         lonLocation = location.coordinate.longitude
     }
 }
-
